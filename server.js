fab94d005aba11622b94cad0d9c54dccd94789d4
Dalton Maurer <dmaurer3394@gmail.com>
2019-12-16 11:56:05 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("app/public"));
+
+require("./app/routes/html-routes.js")(app);
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
